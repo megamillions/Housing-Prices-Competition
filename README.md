c3bd6245a2cce6edd3c5e0368b603aea64c44264
William Duncan <william.philip.duncan@gmail.com>
2020-04-17 15:31:39 -0700

    Exploratory code
    Exploratory code for model. Selecting columns to be used.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Housing Prices Competition
- Build a model to predict housing prices in a data set.
+
+Build a model to predict housing prices in Ames, Iowa.
+
+Dataset and more information can be found here: https://www.kaggle.com/c/home-data-for-ml-course
---
+++ housing_explore.py
@@ -0,0 +1,113 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+# True - testing against train set to measure accuracy.
+# False - testing against test set and save for submission.
+is_accuracy = True
+
+# Path of the file to read.
+iowa_file_path = 'train.csv'
+home_data = pd.read_csv(iowa_file_path)
+
+# Create target object and call it y
+y = home_data.SalePrice
+
+# Features with binary categorical data.
+cat_features = ['Street', 'CentralAir']
+
+# Features with missing data.
+na_features = ['LotFrontage', 'MasVnrArea', 'GarageYrBlt']
+
+# Features with categorical and missing data.
+cat_na_features = ['Alley', 'FireplaceQu', 'GarageFinish',
+                   'GarageQual', 'GarageCond', 'PoolQC',
+                   'Fence']
+
+# Features with some other cleaning probably required.
+# YearRemodAdd - consider subtracting from current year or construct year.
+# ExterQual-BsmtFinType2 - convert from string to numerical value.
+to_clean_features = ['YearRemodAdd', 'ExterQual', 'ExterCond',
+                     'BsmtQual', 'BsmtCond', 'BsmtExposure',
+                     'BsmtFinType1', 'BsmtFinType2', 'HeatingQC',
+                     'KitchenQual', 'PavedDrive']
+
+# Other features with no immediate but an eventual use.
+other_features = ['SaleCondition']
+
+# Features with no need to edit.
+features = ['LotArea', 'OverallQual', 'OverallCond'
+            'YearBuilt', 'BsmtFinSF1', 'BsmtFinSF2',
+            'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF',
+            '2ndFlrSF', 'LowQualFinSF', 'GrLivArea',
+            'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
+            'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr',
+            'TotRmsAbvGrd', 'Fireplaces', 'GarageCars',
+            'GarageArea', 'WoodDeckSF', 'OpenPorchSF',
+            'EnclosedPorch', '3SsnPorch', 'ScreenPorch',
+            'PoolArea', 'MiscVal']
+X = home_data[features]
+
+if is_accuracy:
+    
+    # Split into validation and training data.
+    train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+    
+    # Specify model.
+    iowa_model = DecisionTreeRegressor(random_state=1)
+    
+    # Fit model.
+    iowa_model.fit(train_X, train_y)
+    
+    # Make validation predictions and calculate mean absolute error.
+    val_predictions = iowa_model.predict(val_X)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print("Validation MAE when not specifying max_leaf_nodes: {:,.0f}".
+          format(val_mae))
+    
+    # Using best value for max_leaf_nodes
+    iowa_model = DecisionTreeRegressor(max_leaf_nodes=100, random_state=1)
+    iowa_model.fit(train_X, train_y)
+    val_predictions = iowa_model.predict(val_X)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print("Validation MAE for best value of max_leaf_nodes: {:,.0f}".
+          format(val_mae))
+    
+    # Define the model. Set random_state to 1
+    rf_model = RandomForestRegressor(random_state=1)
+    rf_model.fit(train_X, train_y)
+    rf_val_predictions = rf_model.predict(val_X)
+    rf_val_mae = mean_absolute_error(rf_val_predictions, val_y)
+    
+    print("Validation MAE for Random Forest Model: {:,.0f}".
+          format(rf_val_mae))
+    
+else:
+        
+    # Create a new Random Forest model.
+    rf_model_on_full_data = RandomForestRegressor()
+    
+    # fit rf_model_on_full_data on all data from the training data
+    rf_model_on_full_data.fit(X, y)
+    
+    # path to file you will use for predictions
+    test_data_path = 'test.csv'
+    
+    # read test data file using pandas
+    test_data = pd.read_csv(test_data_path)
+    
+    # Create test_X which comes from test_data but includes only the
+    # columns you used for prediction.
+    # The list of columns is stored in a variable called features
+    test_X = test_data[features]
+    
+    # make predictions which we will submit. 
+    test_preds = rf_model_on_full_data.predict(test_X)
+    
+    # Save predictions in format used for competition.
+    output = pd.DataFrame({'Id': test_data.Id,
+                           'SalePrice': test_preds})
+    
+    output.to_csv('submission.csv', index=False)
